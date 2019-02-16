f036da455892b561f096578fe35f98ea02be52a9
Lets Git Cracking <digitalmailsecure@gmail.com>
2019-02-16 15:58:02 +0000

    Add your favourite movie cover #1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Matrix from './img/matrix.png'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <img src={Matrix} alt="Matrix wallpaper"/>
       </div>
     );
   }
