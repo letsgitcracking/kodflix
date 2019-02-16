c6d1ddf245ea96608df20656a4eb66b56b9d32b3
Lets Git Cracking <digitalmailsecure@gmail.com>
2019-02-16 16:09:12 +0000

    Add 6 TV Show titles #3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
-import Matrix from './img/matrix.png'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <img src={Matrix} alt="Matrix wallpaper"/>
+        <br></br>
+        <br></br>
+        <br></br>
+        <div className="container">
+          <div className="item">Show 1</div>
+          <div className="item">Show 2</div>
+          <div className="item">Show 3</div>
+        </div>
+        <div className="container">
+          <div className="item">Show 4</div>
+          <div className="item">Show 5</div>
+          <div className="item">Show 6</div>
+        </div>
       </div>
     );
   }
