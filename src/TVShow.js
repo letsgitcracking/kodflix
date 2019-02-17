b531296046da45e6950013d2c993beb8bd71bfed
letsgitcracking <45171676+letsgitcracking@users.noreply.github.com>
2019-02-17 09:05:03 +0000

    Keep a unique details URL for each TV show #10

--- src/TVShow.js
+++ src/TVShow.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function TVShow(props) {
-    return <Link to='/details' className="item">
+    return <Link to={`/${props.id}`} className="item">
         <img src={props.logo} alt={`${props.name} Logo`} />
         <div className="overlay">
             <h2>{props.name}</h2>
