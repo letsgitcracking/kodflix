e2a0c56f73bbe68e15cac1d91e5455243fbb097a
Lets Git Cracking <digitalmailsecure@gmail.com>
2019-02-17 15:02:20 +0000

    Improve the gallery layout #11

--- src/Gallery.js
+++ src/Gallery.js
@@ -11,13 +11,11 @@ import StrangerThings from './img/strangerthings_cover.jpg';
 export default function Gallery() {
     return (
         <div>
-            <Header />
+            <div className="container"><Header /></div>
             <div className="container">
                 <TVShow id='maninthehighcastle' name="The Man in the High Castle" logo={HighCastle} />
                 <TVShow id='thebureau' name="The Bureau" logo={Bureau} />
                 <TVShow id='mrrobot' name="Mr. Robot" logo={Robot} />
-            </div>
-            <div className="container">
                 <TVShow id='strangerthings' name="Stranger Things" logo={StrangerThings} />
                 <TVShow id='jackryan' name="Jack Ryan" logo={JackRyan} />
                 <TVShow id='haltandcatchfire' name="Halt and Catch Fire" logo={HaltFire} />
