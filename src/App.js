1551fb00a5fb6ba41c21abc7fe28aad27f0d71a2
letsgitcracking <45171676+letsgitcracking@users.noreply.github.com>
2019-02-17 08:04:59 +0000

    Create a Gallery component to show the covers #9

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import TVShow from './TVShow';
-import Bureau from './img/thebureau_cover.jpg';
-import HighCastle from './img/manhighcastle_cover.jpg';
-import HaltFire from './img/haltcatchfire_cover.jpg';
-import JackRyan from './img/jackryan_cover.jpg';
-import Robot from './img/mrrobot_cover.jpg';
-import StrangerThings from './img/strangerthings_cover.jpg';
+import Gallery from './Gallery';
 import Kodflix from './img/kodflix.png'
 
 
@@ -19,16 +13,7 @@ class App extends Component {
             <img src={Kodflix} alt="Kodflix Logo" />
           </div>
         </div>
-        <div className="container">
-          <TVShow name="The Man in the High Castle" logo={HighCastle} />
-          <TVShow name="The Bureau" logo={Bureau} />
-          <TVShow name="Mr. Robot" logo={Robot} />
-        </div>
-        <div className="container">
-          <TVShow name="Stranger Things" logo={StrangerThings} />
-          <TVShow name="Jack Ryan" logo={JackRyan} />
-          <TVShow name="Halt and Catch Fire" logo={HaltFire} />
-        </div>
+        <Gallery />
       </div>
     );
   }
---
+++ src/Gallery.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import TVShow from './TVShow';
+import Bureau from './img/thebureau_cover.jpg';
+import HighCastle from './img/manhighcastle_cover.jpg';
+import HaltFire from './img/haltcatchfire_cover.jpg';
+import JackRyan from './img/jackryan_cover.jpg';
+import Robot from './img/mrrobot_cover.jpg';
+import StrangerThings from './img/strangerthings_cover.jpg';
+
+export default function Gallery() {
+    return (
+        <div>
+            <div className="container">
+                <TVShow name="The Man in the High Castle" logo={HighCastle} />
+                <TVShow name="The Bureau" logo={Bureau} />
+                <TVShow name="Mr. Robot" logo={Robot} />
+            </div>
+            <div className="container">
+                <TVShow name="Stranger Things" logo={StrangerThings} />
+                <TVShow name="Jack Ryan" logo={JackRyan} />
+                <TVShow name="Halt and Catch Fire" logo={HaltFire} />
+            </div>
+        </div>
+    )
+}
