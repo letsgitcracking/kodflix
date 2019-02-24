cd4bdc1103cda6a349fdff8cb93ee3924a9037cd
letsgitcracking <45171676+letsgitcracking@users.noreply.github.com>
2019-02-24 22:14:53 +0000

    Return a list of shows from the backend #19

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const getTVShows = require('./shows-list');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/rest/shows-list', (req, res) => res.send(getTVShows()))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
