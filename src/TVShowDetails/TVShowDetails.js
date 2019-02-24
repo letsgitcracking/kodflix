792a4c1e1baf349b29e7d9033acaefb20e5da278
letsgitcracking <45171676+letsgitcracking@users.noreply.github.com>
2019-02-24 20:19:19 +0000

    Includes new files

---
+++ src/TVShowDetails/TVShowDetails.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import getTVShows from '../tvshows-get';
+import './TVShowDetails.css';
+
+export default class TVShowDetails extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            TVShow: {}
+        };
+    }
+
+    componentDidMount() {
+        let TVShowDetails = this.props.match.params.TVShowDetails;
+        let TVShow = getTVShows().find(show => show.id === TVShowDetails);
+        this.setState({ TVShow });
+    }
+
+    render() {
+        if (this.state.TVShow === undefined) {
+            return <Redirect to='/not-found' />;
+        } else {
+            return (
+                <div>
+                    <div>
+                        <h1 >{this.state.TVShow.name}</h1>
+                    </div>
+                    <div className='content'>
+                        <img
+                            src={this.state.TVShow.logo}
+                            alt={this.state.TVShow.name} />
+                        <div className='synopsis'>{this.state.TVShow.synopsis}</div>
+                    </div>
+
+                    <Link to='/'>Back to home page</Link>
+                </div>
+            )
+        }
+    }
+}
